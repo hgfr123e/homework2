b1d4e679f702bc8a99bfa3eeda63333e89a1a66e
akameshi <bogdan.kuzmin.08@gmail.com>
2023-01-22 19:15:08 +0600

    Create homework№2.py

---
+++ homework№2.py
@@ -0,0 +1,16 @@
+#Задание №1
+list=[2,7,3]
+list.append(9)
+print(list)
+#Задание №2
+a=[2,3]
+b=[6,1]
+a.extend(b)
+print(a)
+#Задание №3
+a=[1,5,6]
+a.remove(1)
+print(a)
+#Задание №4
+a=[5,1,3,1,9]
+print(a.count(1))
